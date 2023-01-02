1f33c34a45c92951dc90b7992280e3416c1fbf8d
reeshee <reesheeroy@gmail.com>
2023-01-01 23:50:42 -0500

    Add Sorting Functionality

--- script.js
+++ script.js
@@ -1,65 +1,74 @@
-// send a POST request to the server to add a new movie to the database
-const form = document.querySelector('#form');
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  const name = document.querySelector('#name').value;
-  const year = document.querySelector('#year').value;
-  const rating = document.querySelector('#rating').value;
-  fetch('add-movie.php', {
-    method: 'POST',
-    body: JSON.stringify({ name, year, rating }),
-    headers: { 'Content-Type': 'application/json' }
-  })
-    .then(response => response.text())
-    .then(result => {
-      if (result === 'success') {
-        // clear the form and update the table
-        form.reset();
-        updateTable();
-      } else {
-        alert('Error: ' + result);
-      }
-    });
+// movie-logger.js
+
+// Handle form submission
+const form = document.querySelector('#movie-form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const movie = {
+    name: formData.get('name'),
+    releaseYear: formData.get('releaseYear'),
+    rating: formData.get('rating')
+  };
+  addMovie(movie);
+  form.reset();  // Clear the form
 });
 
-// send a GET request to the server to retrieve the list of movies from the database
-function updateTable() {
-  fetch('get-movies.php')
-    .then(response => response.json())
-    .then(movies => {
-      const tbody = document.querySelector('#movies tbody');
-      tbody.innerHTML = '';
-      for (const movie of movies) {
-        const tr = document.createElement('tr');
-        tr.innerHTML = `
-          <td>${movie.name}</td>
-          <td>${movie.year}</td>
-          <td>${movie.rating}</td>
-          <td><button data-id="${movie.id}" class="delete">x</button></td>
-        `;
-        tbody.appendChild(tr);
-      }
-    });
+// Load existing movie data from local storage
+const movies = JSON.parse(localStorage.getItem('movies')) || [];
+renderMovies();
+
+// Add a movie to the list and save to local storage
+function addMovie(movie) {
+  movies.push(movie);
+  localStorage.setItem('movies', JSON.stringify(movies));
+  renderMovies();
 }
-updateTable();
 
-// send a DELETE request to the server to remove a movie from the database
-const tbody = document.querySelector('#movies tbody');
-tbody.addEventListener('click', e => {
-  if (e.target.classList.contains('delete')) {
-    const id = e.target.dataset.id;
-    fetch('delete-movie.php', {
-      method: 'DELETE',
-      body: JSON.stringify({ id }),
-      headers: { 'Content-Type': 'application/json' }
-    })
-      .then(response => response.text())
-      .then(result => {
-        if (result === 'success') {
-          updateTable();
-        } else {
-          alert('Error: ' + result);
-        }
-      });
+// Delete a movie from the list and save to local storage
+function deleteMovie(index) {
+  movies.splice(index, 1);
+  localStorage.setItem('movies', JSON.stringify(movies));
+  renderMovies();
+}
+
+// Render the movie list to the page
+function renderMovies(sortField = '', sortDirection = '') {
+  const movieTable = document.querySelector('#movie-table tbody');
+  movieTable.innerHTML = '';
+  // Sort the movies array
+  if (sortField && sortDirection) {
+    movies.sort((a, b) => {
+      if (sortField === 'name') {
+        return sortDirection === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
+      } else if (sortField === 'releaseYear') {
+        return sortDirection === 'asc' ? a.releaseYear - b.releaseYear : b.releaseYear - a.releaseYear;
+      } else if (sortField === 'rating') {
+        return sortDirection === 'asc' ? a.rating - b.rating : b.rating - a.rating;
+      }
+    });
   }
-});
+  // Populate the table with the sorted movies array
+  for (let i = 0; i < movies.length; i++) {
+    const movie = movies[i];
+    const tr = document.createElement('tr');
+    const nameTd = document.createElement('td');
+    nameTd.textContent = movie.name;
+    tr.appendChild(nameTd);
+    const releaseYearTd = document.createElement('td');
+    releaseYearTd.textContent = movie.releaseYear;
+    tr.appendChild(releaseYearTd);
+    const ratingTd = document.createElement('td');
+    ratingTd.textContent = movie.rating;
+    tr.appendChild(ratingTd);
+    const deleteTd = document.createElement('td');
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'x';
+    deleteButton.addEventListener('click', () => {
+      deleteMovie(i);
+    });
+    deleteTd.appendChild(deleteButton);
+    tr.appendChild(deleteTd);
+    movieTable.appendChild(tr);
+  }
+}
